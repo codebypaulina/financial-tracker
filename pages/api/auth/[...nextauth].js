c3804f119e609c34a93951060f2e53b291156ec6
codebypaulina <paulinaczeschla@web.de>
2024-10-07 17:11:26 +0200

    instal mongoDB Adapter and create nextauth.js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/db/mongodb";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+
+  debug: true,
+
+  callbacks: {
+    async session({ session, user }) {
+      session.user.userId = user.id;
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
