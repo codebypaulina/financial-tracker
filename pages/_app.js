a2bc9da94e6e96eee0516558aac5e17c6f7a6abb
codebypaulina <paulinaczeschla@web.de>
2024-10-07 17:16:15 +0200

    wrap everything in session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,26 @@
 import GlobalStyle from "../styles";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
+    <SessionProvider session={session}>
       <GlobalStyle />
       <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
+}
+
+
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    
+      <Component {...pageProps} />
+    
+  )
 }
