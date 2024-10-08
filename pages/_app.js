984b646806208a7d9b42b67f55d672351d41f691
codebypaulina <paulinaczeschla@web.de>
2024-10-08 22:56:49 +0200

    added SWR setup to _app.js for data fetching configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import GlobalStyle from "../styles";
 import { SessionProvider } from "next-auth/react";
+import { SWRConfig } from "swr";
 
 export default function App({
   Component,
@@ -7,10 +8,22 @@ export default function App({
 }) {
   return (
     <>
-      <SessionProvider session={session}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </SessionProvider>
+      <SWRConfig
+        value={{
+          fetcher: async (...args) => {
+            const response = await fetch(...args);
+            if (!response.ok) {
+              throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+            }
+            return await response.json();
+          },
+        }}
+      >
+        <SessionProvider session={session}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </SessionProvider>
+      </SWRConfig>
     </>
   );
 }
