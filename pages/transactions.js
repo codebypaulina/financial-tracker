69ed0c0c07ac4960b6e84d8a003ea2ffb86dce94
codebypaulina <paulinaczeschla@web.de>
2024-10-08 22:40:15 +0200

    set up status for transaction data on TransactionsPage

--- pages/transactions.js
+++ pages/transactions.js
@@ -1,6 +1,10 @@
 import Navbar from "@/components/Navbar";
+import { useState, useEffect } from "react";
 
 export default function TransactionsPage() {
+  const [transactions, setTransactions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div>
       <h1>Transactions</h1>
