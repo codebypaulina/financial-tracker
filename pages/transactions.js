1e337f697c75c00c96982b9db6a18ca450aaebbc
codebypaulina <paulinaczeschla@web.de>
2024-10-08 22:08:00 +0200

    added basic structure for transactions page

---
+++ pages/transactions.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+export default function TransactionsPage() {
+  return (
+    <div>
+      <h1>Transactions</h1>
+
+      <Navbar />
+    </div>
+  );
+}
