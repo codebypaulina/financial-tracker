c8faa70ac2b460792a2b39c8c696c481835fb126
codebypaulina <paulinaczeschla@web.de>
2024-10-08 23:03:57 +0200

    added SWR for data fetching on TransactionsPage

--- pages/transactions.js
+++ pages/transactions.js
@@ -1,15 +1,24 @@
 import Navbar from "@/components/Navbar";
-import { useState, useEffect } from "react";
+import useSWR from "swr";
 
 export default function TransactionsPage() {
-  const [transactions, setTransactions] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { data: transactions, error } = useSWR("/api/transactions");
+
+  if (error) return <h3>Failed to load transactions</h3>;
+  if (!transactions) return <h3>Loading...</h3>;
 
   return (
-    <div>
+    <>
       <h1>Transactions</h1>
 
+      <ul>
+        {transactions.map((transaction) => (
+          <li key={transaction._id}>
+            {transaction.description} - {transaction.amount}€
+          </li>
+        ))}
+      </ul>
       <Navbar />
-    </div>
+    </>
   );
 }
