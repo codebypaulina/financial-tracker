6e7a488efa67ef36158889649614cd0ac89af94a
codebypaulina <paulinaczeschla@web.de>
2024-10-09 13:06:10 +0200

    added SWR for data fetching on CategoriesPage to display category details

--- pages/categories.js
+++ pages/categories.js
@@ -1,9 +1,24 @@
 import Navbar from "@/components/Navbar";
+import useSWR from "swr";
 
-export default function TransactionsPage() {
+export default function CategoriesPage() {
+  const { data: categories, error } = useSWR("/api/categories");
+
+  if (error) return <h3>Failed to load categories</h3>;
+  if (!categories) return <h3>Loading...</h3>;
+
   return (
     <>
       <h1>Categories</h1>
+
+      <ul>
+        {categories.map((category) => (
+          <li key={category._id}>
+            <strong>{category.name}</strong> | {category.type} |{" "}
+            <span>{category.totalAmount || 0} €</span>
+          </li>
+        ))}
+      </ul>
 
       <Navbar />
     </>
--- pages/transactions.js
+++ pages/transactions.js
@@ -22,6 +22,7 @@ export default function TransactionsPage() {
           </li>
         ))}
       </ul>
+
       <Navbar />
     </>
   );
