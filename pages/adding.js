2a70ea25365a32363deeec36fa2e0ef735d35094
codebypaulina <paulinaczeschla@web.de>
2024-10-09 17:04:13 +0200

    added FormAddTransaction component with basic structure & implemented to AddingPage

--- pages/adding.js
+++ pages/adding.js
@@ -1,9 +1,12 @@
 import Navbar from "@/components/Navbar";
+import FormAddTransaction from "@/components/FormAddTransaction";
 
 export default function AddingPage() {
   return (
     <>
       <h1>Add Transaction / Category</h1>
+
+      <FormAddTransaction />
 
       <Navbar />
     </>
