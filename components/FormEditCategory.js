97170b746d240f3097e7543b3d0e28f5a908c6ab
codebypaulina <paulinaczeschla@web.de>
2024-10-10 23:03:13 +0200

    implemented form handling to FormEditCategory (PUT request to API / save functionality to update category data)

--- components/FormEditCategory.js
+++ components/FormEditCategory.js
@@ -8,8 +8,35 @@ export default function FormEditCategory() {
     router.back(); // zurück zur vorherigen Seite (nochmal überdenken, ob er nicht lieber Formular clearen soll & zustätzl. X-Button dafür implemetieren)
   }
 
+  // Save
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    try {
+      const response = await fetch(`/api/categories/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log("UPDATING SUCCESSFUL! (category)");
+        router.back(); // nach erfolgreichem Updaten der Kategorie zurück zur vorherigen Seite
+      } else {
+        throw new Error("Failed to update category");
+      }
+    } catch (error) {
+      console.error("Error updating category: ", error);
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Edit Category</h2>
 
       <label htmlFor="type">Type:</label>
