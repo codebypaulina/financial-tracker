023d38ec1e21a2f270b999d937853c8dca1ff9ef
codebypaulina <paulinaczeschla@web.de>
2024-10-10 16:36:02 +0200

    added useRouter to TransactionDetailsPage to fetch transaction-id for editing in FormEditTransaction

--- pages/transactions/[id].js
+++ pages/transactions/[id].js
@@ -1,5 +1,9 @@
 import FormEditTransaction from "@/components/FormEditTransaction";
+import { useRouter } from "next/router";
 
 export default function TransactionDetailsPage() {
-  return <FormEditTransaction />;
+  const router = useRouter();
+  const { id } = router.query; // ID der entspr. transaction aus URL extrahiert
+
+  return <FormEditTransaction transactionId={id} />; // ID als prop weitergegeben
 }
