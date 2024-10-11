6877900addac1dd03974989c2022473e88a18b5d
codebypaulina <paulinaczeschla@web.de>
2024-10-11 14:33:04 +0200

    integrated useSWR hook on HomePage to fetch transactions data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import LoginSection from "@/components/LoginSection";
 import Navbar from "@/components/Navbar";
+import useSWR from "swr";
 
 export default function HomePage() {
+  const { data: transactions, error } = useSWR("/api/transactions");
+
+  if (error) return <h3>Failed to load transactions</h3>;
+  if (!transactions) return <h3>Loading...</h3>;
+
   return (
     <div>
       <h1>Finance Tracker</h1>
