dbfb6af9de5649c31dfe6a64bd19fb7c48cf7484
codebypaulina <paulinaczeschla@web.de>
2024-10-12 18:23:21 +0200

    added basic structure for CategoryDetailsPage

--- pages/categories/[id].js
+++ pages/categories/[id].js
@@ -1,9 +1,24 @@
 import FormEditCategory from "@/components/FormEditCategory";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 export default function CategoryDetailsPage() {
   const router = useRouter();
   const { id } = router.query; // ID der entspr. category aus URL extrahiert
 
-  return <FormEditCategory categoryId={id} />; // ID als prop weitergegeben
+  const { data: category, error } = useSWR(id ? `/api/categories/${id}` : null);
+
+  if (error) return <h3>Failed to load category</h3>;
+  if (!category) return <h3>Loading...</h3>;
+
+  return (
+    <>
+      <h2>Category Datails: {category.name}</h2>
+      <ul>
+        <li>für Transaktionenliste</li>
+      </ul>
+
+      <FormEditCategory categoryId={id} />
+    </>
+  );
 }
