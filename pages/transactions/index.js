7c58fabf0ea059c2627858e1c3e7a3446359ea72
codebypaulina <paulinaczeschla@web.de>
2024-10-13 18:08:31 +0200

    added chart to TransactionsPage using same data as on CategoriesPage main view

--- pages/transactions/index.js
+++ pages/transactions/index.js
@@ -2,16 +2,83 @@ import Navbar from "@/components/Navbar";
 import useSWR from "swr";
 import Link from "next/link";
 import styled from "styled-components";
+import dynamic from "next/dynamic";
+
+// dynamisch, sonst ES Module error (auch bei aktuellster next.js-Version)
+const ResponsivePie = dynamic(
+  () => import("@nivo/pie").then((mod) => mod.ResponsivePie),
+  { ssr: false }
+);
 
 export default function TransactionsPage() {
-  const { data: transactions, error } = useSWR("/api/transactions");
+  const { data: transactions, error: errorTransactions } =
+    useSWR("/api/transactions");
+  const { data: categories, error: errorCategories } =
+    useSWR("/api/categories");
 
-  if (error) return <h3>Failed to load transactions</h3>;
-  if (!transactions) return <h3>Loading...</h3>;
+  if (errorTransactions || errorCategories) return <h3>Failed to load data</h3>;
+  if (!transactions || !categories) return <h3>Loading...</h3>;
+
+  // chart
+  const totalIncome = categories
+    .filter((category) => category.type === "Income")
+    .reduce((sum, category) => sum + category.totalAmount, 0);
+
+  const totalExpense = categories
+    .filter((category) => category.type === "Expense")
+    .reduce((sum, category) => sum + category.totalAmount, 0);
+
+  const remainingIncome = totalIncome - totalExpense;
+
+  const chartData = [
+    {
+      id: "Expenses",
+      label: "Expenses",
+      value: totalExpense,
+      color: "var(--expense-color)",
+    },
+    {
+      id: "Remaining Income",
+      label: "Remaining Income",
+      value: remainingIncome,
+      color: "var(--income-color)",
+    },
+  ];
 
   return (
     <>
       <h1>Transactions</h1>
+      <h2>
+        Total Balance:{" "}
+        {remainingIncome.toLocaleString("de-DE", {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}{" "}
+        €
+      </h2>
+
+      {chartData.length > 0 && (
+        <ChartSection>
+          <ResponsivePie
+            data={chartData}
+            colors={{ datum: "data.color" }} // nutzt definierte Farben für Segmente
+            innerRadius={0.5} // 50 % ausgeschnitten
+            padAngle={2} // Abstand zwischen Segmenten
+            cornerRadius={3} // rundere Ecken der Segmente
+            arcLinkLabelsSkipAngle={360} // ausgeblendete Linien
+            animate={false} // Segmente springen nicht
+            enableArcLabels={false} // keine Zahlen im Segment
+            tooltip={({ datum }) => {
+              const percentage = ((datum.value / totalIncome) * 100).toFixed(0);
+              return (
+                <div>
+                  {datum.label}: <strong>{percentage} %</strong>
+                </div>
+              );
+            }}
+          />
+        </ChartSection>
+      )}
 
       <ul>
         {transactions.map((transaction) => (
@@ -34,6 +101,11 @@ export default function TransactionsPage() {
     </>
   );
 }
+
+const ChartSection = styled.div`
+  height: 200px;
+  width: 200px;
+`;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
