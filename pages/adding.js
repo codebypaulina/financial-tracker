556f12c013d5fedb36baf2d04b0cbff75617b20f
codebypaulina <paulinaczeschla@web.de>
2026-01-03 20:34:59 +0100

    add basic layout & button styling to AddingPage

--- pages/adding.js
+++ pages/adding.js
@@ -4,12 +4,13 @@ import { useState } from "react"; // für selection
 import Navbar from "@/components/Navbar";
 import FormAddTransaction from "@/components/FormAddTransaction";
 import FormAddCategory from "@/components/FormAddCategory";
+import styled from "styled-components";
 
 export default function AddingPage() {
   const [selection, setSelection] = useState(null);
 
   return (
-    <>
+    <PageWrapper>
       {!selection && (
         <h1>
           Do you want to add a{" "}
@@ -25,6 +26,27 @@ export default function AddingPage() {
       {selection === "category" && <FormAddCategory />}
 
       <Navbar />
-    </>
+    </PageWrapper>
   );
 }
+
+const PageWrapper = styled.div`
+  min-height: 100vh; // Wrapper nimmt mind. volle Bildschirmhöhe ein
+  display: flex; // zentriert Inhalt
+  justify-content: center; // h1 horizontal zentriert
+  align-items: center; // h1 vertikal zentriert
+  padding: 2rem; // Abstand zum Bildschirmrand
+
+  button {
+    border: none;
+    border-radius: 20px;
+    min-width: 90px;
+    min-height: 30px;
+    cursor: pointer;
+
+    &:hover {
+      transform: scale(1.07);
+      font-weight: bold;
+    }
+  }
+`;
